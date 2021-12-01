84e0acfefbedf943137d7d3af8babd6269c129d8
evasotog <84499401+evasotog@users.noreply.github.com>
2021-12-01 18:56:43 +0100

    Update scriptcompare.py

--- scriptcompare.py
+++ scriptcompare.py
@@ -4,22 +4,30 @@
 import re
 import numpy as np
 
-
-with open ('pr2-21-22.txt') as f:
-
-    file=f.read()
-    funcion= re.findall(r'(?<=] )(.*)(?=.DLL)', "stringconelquecomparar" )
-    
-    funciones = np.array(funcion)
-
-print(funciones.grep(Regexp.union(readFile("maliciosas.txt"))))
-
-f.close()
-
 def readFile(fileName):
         fileObj = open(fileName, "r") 
         words = fileObj.read().splitlines() #esta funcion pone el txt en un array
         fileObj.close()
         return words
+
+
+with open ('pr2-21-22.txt',"r") as f:
+
+    file=f.read()
+    funcion= re.findall(r'(?<=] )(.*)(?=.DLL)', file) #funcion es una lista
+  
+    array = readFile("maliciosas.txt")
+    listamal = np.array(array).tolist()
+    print(listamal)
+
+    results = {}
+    for i in listamal:
+        results[i] = funcion.count(i) 
+
+print("###################################################################################################################################")
+print(results)
+
+
+f.close()
 
 
